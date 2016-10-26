f79e1294a4e8abf49a8b0d3a7413eff3de2e6bd3
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2016-10-26 14:31:42 -0400

    Add move and valid_move? spec

--- lib/towers_of_hanoi.rb
+++ lib/towers_of_hanoi.rb
@@ -5,4 +5,15 @@ class TowersOfHanoi
   def initialize
     @towers = ([[3, 2, 1], [], []])
   end
+
+  def move(start_tower, end_tower)
+    @towers[end_tower] << @towers[start_tower].pop
+  end
+
+  def valid_move?(start_tower, end_tower)
+    return false if @towers[start_tower].empty?
+    return true if @towers[end_tower].empty?
+    return false if @towers[start_tower].last > @towers[end_tower].last
+    true
+  end
 end
--- spec/towers_of_hanoi_spec.rb
+++ spec/towers_of_hanoi_spec.rb
@@ -11,7 +11,28 @@ describe TowersOfHanoi do
   describe '#initialize' do
     it "should initialize towers" do
       expect(game.towers).to eql([[3, 2, 1], [], []])
-      
+    end
+  end
+
+  describe '#move' do
+    it "should move disc" do
+      game.move(0, 1)
+      expect(game.towers).to eq([[3, 2], [1], []])
+    end
+  end
+
+  describe '#valid_move?' do
+    it "should return true if it is a valid move" do
+      expect(game.valid_move?(0, 2)).to be true
+    end
+
+    it "should return false if it is an invalid move" do
+      expect(game.valid_move?(2, 0)).to be false
+    end
+
+    it "should return false if start disc is greater than end disc" do
+      game.move(0, 1)
+      expect(game.valid_move?(0, 1)). to be false
     end
   end
 
